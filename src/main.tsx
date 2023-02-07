3214396d5e83a2823ac69b1cf3dd26fb5fe6c1eb
Gaurav Joshi <gj451026@gmail.com>
2023-02-07 13:58:26 +0530

    Folder structure and SCSS added

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import './styling/index.scss'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
