5a8f830baf31ab02f16c8a8495683e62a23a99fb
Gaurav Joshi <gj451026@gmail.com>
2023-02-09 15:43:49 +0530

    Sign in strucutre added

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
-import React from 'react'
-import { Routes, Route } from "react-router-dom"
-import './styling/App.scss'
-import LoginPage from './pages/LoginPage'
-import SignupPage from './pages/SignupPage'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import "./styling/App.scss";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
 
 function App() {
-
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={ <LoginPage/> } />
-        <Route path="/signup" element={ <SignupPage/> } />
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,17 +1,45 @@
-import React from 'react'
-import "../styling/LoginPage.scss"
-import Logo from "../components/Logo"
-
+import React from "react";
+import "../styling/LoginPage.scss";
+import Logo from "../components/Logo";
+import { Link } from "react-router-dom";
 
 const LoginPage = () => {
   return (
-    <div className='LoginPage__container'>
+    <div className="LoginPage__container">
       <Logo />
       <div className="LoginPage__signin">
-        
+        <div className="LoginPage__signin-welcome">
+          <p>Welcome to VerifyME</p>
+        </div>
+        <div className="LoginPage__signin-signin">
+          <h1>Sign in</h1>
+        </div>
+        <div className="LoginPage__signin-google">
+          <p>Sign in with Google</p>
+        </div>
+        <div className="LoginPage__signin-up">
+          <div className="LoginPage__signin-username">
+            <p>Enter you username or email address</p>
+            <input type="text" />
+          </div>
+          <div className="LoginPage__signin-password">
+            <p>Enter your password</p>
+            <input type="password" />
+          </div>
+        </div>
+        <div className="LoginPage__forgot-password">
+          <p>Forgot Password</p>
+        </div>
+        <div className="LoginPage__login-button">
+          <button type="submit">Sign in</button>
+        </div>
+        <div className="LoginPage__link-signup">
+          <p>No Account?</p>
+          <Link to="/signup">Sign up</Link>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default LoginPage
+export default LoginPage;
