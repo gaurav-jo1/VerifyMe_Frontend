1a16d560c04e09465a67e47cc9325e81ca2939fc
Gaurav Joshi <gj451026@gmail.com>
2023-02-10 00:40:55 +0530

    Sign in styling completed

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "../styling/LoginPage.scss";
 import Logo from "../components/Logo";
+import Google from "../assets/google.png";
 import { Link } from "react-router-dom";
 
 const LoginPage = () => {
@@ -9,26 +10,29 @@ const LoginPage = () => {
       <Logo />
       <div className="LoginPage__signin">
         <div className="LoginPage__signin-welcome">
-          <p>Welcome to VerifyME</p>
+          <p>
+            Welcome to <span>VerifyME</span>
+          </p>
         </div>
         <div className="LoginPage__signin-signin">
           <h1>Sign in</h1>
         </div>
         <div className="LoginPage__signin-google">
-          <p>Sign in with Google</p>
+          <img src={Google} alt="google" height={16} width={16} />
+          <p>&nbsp; Sign in with Google</p>
         </div>
         <div className="LoginPage__signin-up">
           <div className="LoginPage__signin-username">
             <p>Enter you username or email address</p>
-            <input type="text" />
+            <input type="text" placeholder="Username or email address"/>
           </div>
           <div className="LoginPage__signin-password">
             <p>Enter your password</p>
-            <input type="password" />
+            <input type="password" placeholder="Password" />
           </div>
         </div>
         <div className="LoginPage__forgot-password">
-          <p>Forgot Password</p>
+          <p>Forgot Password?</p>
         </div>
         <div className="LoginPage__login-button">
           <button type="submit">Sign in</button>
