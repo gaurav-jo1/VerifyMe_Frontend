f504f17a5c73c4da41abd7a2b8a84ccd020b80d3
Gaurav Joshi <gj451026@gmail.com>
2023-02-10 22:27:51 +0530

    Signup styling and Responsive added

--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -1,43 +1,43 @@
 import React from "react";
 import Logo from "../components/Logo";
-import "../styling/SignupPage.scss";
+import "../styling/LoginPage.scss";
 import { Link } from "react-router-dom";
 
 const SignupPage = () => {
   return (
-    <div className="SignupPage__container display__flex">
+    <div className="LoginPage__container display__flex">
       <Logo />
-      <div className="SingupPage__signup">
-        <div className="SingupPage__signup-welcome">
-          <p>Welcome to VerifyME</p>
+      <div className="LoginPage__signin LoginPage__signup">
+        <div className="LoginPage__signin-welcome">
+          <p>Welcome to <span>VerifyME</span></p>
         </div>
-        <div className="SingupPage__signup-signup">
+        <div className="LoginPage__signin-signin">
           <h1>Sign up</h1>
         </div>
-        <div className="SignupPage__signup-userinfo">
-          <div className="SignupPage__signup-email">
+        <div className="LoginPage__signup-up">
+          <div className="LoginPage__signin-username">
             <p>Enter you username or email address</p>
             <input type="text" />
           </div>
           <div className="SignupPage__signup_username_name">
-            <div className="SignupPage__signup-userinfo_username">
+            <div className="LoginPage__signin-username">
               <p>Enter you username</p>
               <input type="text" />
             </div>
-            <div className="SignupPage__signup-userinfo_name">
+            <div className="LoginPage__signin-username">
               <p>Enter you name</p>
               <input type="text" />
             </div>
           </div>
-          <div className="SignupPage__signup-password">
+          <div className="LoginPage__signin-password">
             <p>Enter your password</p>
             <input type="password" />
           </div>
         </div>
-        <div className="SignupPage__signup-button">
-          <button type="submit">Sign in</button>
+        <div className="LoginPage__login-button">
+          <button type="submit">Sign un</button>
         </div>
-        <div className="SignupPage__signup-signin">
+        <div className="LoginPage__link-signup">
           <p>Have an Account?</p>
           <Link to="/">Sign in</Link>
         </div>
